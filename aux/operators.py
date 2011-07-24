864eaa5b1c815ce95ef8e49f7c53e5912eee2f20
DrBones <rogue.inbox@yahoo.de>
2011-07-25 00:35:24 +0200

    forgot .gitignore and the new operators.py

---
+++ aux/operators.py
@@ -0,0 +1,15 @@
+def spindens(self,lrgm_out):
+    from scipy import split,pi
+    Gup, Gdown = split(lrgm_out.reshape(self.wafer.shape[0],self.wafer.shape[1]*2),2,axis=1)
+    Sz = self.hbar/(4*pi*1j*self.a**2)*(Gup-Gdown)
+    print 'max Spin Split: ', Sz.imag.max()-Sz.imag.min()
+    return Sz.imag
+
+def edens(self,lrgm_out):
+    from scipy import split,pi
+    if self.multi ==1:
+        edensity = lrgm_out.reshape(self.wafer.shape)*2/(2*pi*self.a**2) #times 2 for spin
+    if self.multi ==2:
+        Gup, Gdown = split(lrgm_out.reshape(self.wafer.shape[0],self.wafer.shape[1]*2),2,axis=1)
+        edensity = 1/(2*pi*self.a**2)*(Gup+Gdown)
+    return edensity.real
