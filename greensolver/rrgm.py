b3ffd24f4f7912422a8af54d9ed72c92c03f2f9f
DrBones <rogue.inbox@yahoo.de>
2011-09-05 12:19:38 +0200

    Gotten rid of the minus, now the code conforms to e.g. Nikolic, Yabro Souma

--- greensolver/rrgm.py
+++ greensolver/rrgm.py
@@ -3,19 +3,23 @@ def rrgm(Ablock):
     the retarded green's function, uses views on A, i.e. the block
     matrices of A, by Ablock """
     from scipy import array, hstack
+    from collections import OrderedDict
     number_of_blocks = len(Ablock)
-    grl = [Ablock[0,0].todense().I]
-    prev_greensfnc = grl[0]
+    grl = OrderedDict()
+    grl[0,0] = Ablock[0,0].todense().I
+    prev_greensfnc = grl[0,0]
     for i in range(1,number_of_blocks):
         prev_greensfnc = (Ablock[i,i]-Ablock[i, i-1] * prev_greensfnc * Ablock[i-1,i]).I
-        grl.append(prev_greensfnc)
+        grl[i,i] = prev_greensfnc
+        grl[i,0] = grl[i,i]*Ablock[i,i-1]*grl[i-1,0]
+        #Gr only needed in final calculations of lesser Gr, possible speed gain
     Gr = {}
-    Gr[number_of_blocks-1,number_of_blocks-1] = grl[-1]
+    Gr[number_of_blocks-1,number_of_blocks-1] = grl[(number_of_blocks-1,number_of_blocks-1)]
     rev_iterator = reversed(range(1,number_of_blocks))
     for i in rev_iterator:
-        Gr[i, i-1] = -Gr[i,i] * Ablock[i,i-1] * grl[i-1]
-        Gr[i-1, i] = -grl[i-1] * Ablock[i-1,i] * Gr[i,i]
-        Gr[i-1, i-1] = grl[i-1]-grl[i-1] * Ablock[i-1,i] * Gr[i, i-1]
+        Gr[i, i-1] = Gr[i,i] * Ablock[i,i-1] * grl[i-1,i-1]
+        Gr[i-1, i] = grl[i-1,i-1] * Ablock[i-1,i] * Gr[i,i]
+        Gr[i-1, i-1] = grl[i-1,i-1]+grl[i-1,i-1] * Ablock[i-1,i] * Gr[i, i-1]
     green_diag = array([])
     for i in range(number_of_blocks):
         diag = array(Gr[i,i].diagonal()).reshape(-1)
