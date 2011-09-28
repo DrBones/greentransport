e9a6ee73d72a7e45b1a1664060d61d9272cf595d
DrBones <rogue.inbox@yahoo.de>
2011-09-28 22:09:51 +0200

    edens and spindens operators work at least for sparse colinear structures

--- aux/operators.py
+++ aux/operators.py
@@ -1,15 +1,30 @@
 def spindens(self,lrgm_out):
-    from scipy import split,pi
-    if self.order == 'even':
-        Gup, Gdown = split(lrgm_out.reshape(self.wafer.shape[0],self.wafer.shape[1]*2),2,axis=1)
-    if self.order == 'odd':
-        Gup, Gdown =  split(lrgm_out.reshape(-1,2),2,axis=1)
-        Gup, Gdown =  Gup.reshape(self.wafer.shape), Gdown.reshape(self.wafer.shape)
+    from scipy import split,pi,complex128,zeros,repeat
+    number_of_lattice_points = self.wafer.shape[0]*self.wafer.shape[1]
+    number_of_nodes = len(self.active_coords)
+    if number_of_nodes  == number_of_lattice_points:
+        if self.order == 'even':
+            Gup, Gdown = split(lrgm_out.reshape(self.wafer.shape[0],self.wafer.shape[1]*2),2,axis=1)
+        if self.order == 'odd':
+            Gup, Gdown =  split(lrgm_out.reshape(-1,2),2,axis=1)
+            Gup, Gdown =  Gup.reshape(self.wafer.shape), Gdown.reshape(self.wafer.shape)
+        else:
+            print "Please specify order of Nodes, i.e 'even' for allspinup-allspindown per sclice or odd for spinup-spindown-spinup-..."
+        Sz = self.hbar/(4*pi*1j*self.a**2)*(Gup-Gdown)
+    elif number_of_nodes < number_of_lattice_points:
+            Sz= zeros(self.wafer.shape,dtype=complex128)
+            print 'calculating spin density for sparse structure'
+            lrgm_out = self.hbar/(4*pi*1j*self.a**2)*lrgm_out
+            expanded_array_of_coords = repeat(self.tuple_of_coords,2,axis=0)
+            for node_name in self.nodelist:
+                if node_name % 2 == 0:
+                    sign = 1
+                else:
+                    sign = -1
+                Sz[tuple(expanded_array_of_coords[node_name])] += sign * lrgm_out[node_name]
     else:
-        print "Please specify order of Nodes, i.e 'even' for allspinup-allspindown per sclice or odd for spinup-spindown-spinup-..."
-
-    Sz = self.hbar/(4*pi*1j*self.a**2)*(Gup-Gdown)
-    print 'max Spin Split: ', Sz.imag.max()-Sz.imag.min()
+        print 'Number of nodes larger than canvas, something is wrong!'
+    print 'max Spin Split: ', Sz.real.max()-Sz.real.min()
 #Realteil scheint wahrscheinlicher, imag oszilliert wie bloed
     return Sz.real
 
@@ -30,16 +45,19 @@ def edens(self,lrgm_out):
             else:
                 print "Please specify order of Nodes, i.e 'even' for allspinup-allspindown per sclice or odd for spinup-spindown-spinup-..."
     elif number_of_nodes < number_of_lattice_points:
-        from scipy import zeros,complex128
+        from scipy import zeros,complex128,repeat
         edensity = zeros(self.wafer.shape,dtype=complex128)
         if self.multi ==1:
+            print 'calculating electron density without SO'
+            lrgm_out = lrgm_out *2/(2*pi*self.a**2)
             for node_name in self.nodelist:
-                edensity[self.tuple_of_coords[node_name]]= lrgm_out[node_name]*2/(2*pi*self.a**2)
+                edensity[self.tuple_of_coords[node_name]]= lrgm_out[node_name]
         if self.multi ==2:
-            print 'multi =2'
-            lrgm_out = sum(lrgm_out.reshape(-1,2), axis=1)
+            print 'calculating electron density with SO'
+            lrgm_out = lrgm_out*1/(2*pi*self.a**2)
+            expanded_array_of_coords = repeat(self.tuple_of_coords,2,axis=0)
             for node_name in self.nodelist:
-                edensity[self.tuple_of_coords[node_name]]= lrgm_out[node_name]*1/(2*pi*self.a**2)
+                edensity[tuple(expanded_array_of_coords[node_name])] += lrgm_out[node_name]
     else:
         print 'Number of nodes larger than canvas, something is wrong!'
     print 'max Electron Density: ', edensity.max()
