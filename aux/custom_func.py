78d9b353753d81d22659ca2c6be5210d8a68760d
DrBones <rogue.inbox@yahoo.de>
2011-11-20 13:18:16 +0100

    added offset to suppressor function

--- aux/custom_func.py
+++ aux/custom_func.py
@@ -109,6 +109,6 @@ def all_elements_are_unique(array):
 def nodenames_of_contact(lead_graph_list, contact):
     pass
 
-def suppressor(x,region,length):
-    suppress = (x*1.0)/region * heaviside(region/2.0 - abs(x+1-region/2.0)) + heaviside(length/2.0 - region - abs(x - length/2.0)) - ((x-length*1.0)/region)*heaviside(region/2.0 - abs((x-1 - length) + region/2.0))
+def suppressor(x,region,length,offset):
+    suppress = (x*1.0-offset+1)/region * heaviside(region/2.0 - abs(x-offset+1-region/2.0)) + heaviside(length/2.0 - offset - region - abs(x - length/2.0)) - ((x - length*1.0 + offset-1)/region)*heaviside(region/2.0 - abs((x-1 - length + offset) + region/2.0))
     return suppress
