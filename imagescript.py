e092f639501b6f57ba517bc653295d2027f7bb4c
DrBones <rogue.inbox@yahoo.de>
2011-11-29 21:57:31 +0100

    scripts to generate edens validation plots

--- imagescript.py
+++ imagescript.py
@@ -25,8 +25,8 @@ params = {'backend': 'pdf',
           'axes.labelsize': 10,
           'text.fontsize': 10,
           'legend.fontsize': 10,
-          'xtick.labelsize': 8,
-          'ytick.labelsize': 8,
+          'xtick.labelsize': 10,
+          'ytick.labelsize': 10,
           # 'text.dvipnghack' : True,
           'text.usetex': True,
           'figure.figsize': fig_size}
@@ -35,14 +35,14 @@ y1 = psi(x,1)/(psi(x,1).max())
 y2 = y1**2
 y3 = dens1[100,x]
 y4 = y2*y3.max()
-xshift=-0.12
+xshift=-0.13
 # Plot data
 # -------------------------------------
 pl.figure()
 pl.clf()
 pl.axes([0.15,0.2,0.95-0.15,0.95-0.2])
-pl.plot(x,y1,'g:',label=r'$\Psi$')
-pl.plot(x,y2,'-b',label=r'$\Psi^+\Psi$')
+pl.plot(x,y1,'g:',label=r'$\psi$')
+pl.plot(x,y2,'-b',label=r'$\psi^*\psi$')
 pl.xlabel(r'$y$ [nm]')
 pl.ylabel(r'$\pi$ [arb. units]')
 ax = pl.gca()
@@ -53,15 +53,15 @@ pl.savefig('images/analytical1.pdf',transparent='true')
 pl.figure()
 pl.clf()
 pl.axes([0.15,0.2,0.95-0.15,0.95-0.2])
-pl.plot(x,y4,'-b',label='$n1_{analytical}$')
-pl.plot(x,y3,'r2',label='$n1_{simulated}$',markersize=3)
+pl.plot(x,y4,'-b',label='$n1_{ana}$')
+pl.plot(x,y3,'r2',label='$n1_{sim}$',markersize=3)
 pl.xlabel(r'$y$ [nm]')
-pl.ylabel(r'$n$ [1/m$^2$]')
+pl.ylabel(r'$n$ [1/m$^2$] $\times 10^{17}$')
 ax = pl.gca()
 ax.yaxis.set_label_coords(xshift, 0.5)
 locs,labels = pl.yticks()
 pl.yticks(locs, map(lambda x: r"$ %.1f $" % x, locs*1e-17))
-pl.text(-0.17, 0.9, r'$\times 10^{17}$', fontsize=8, transform = pl.gca().transAxes)
+# pl.text(-0.17, 0.9, r'$\times 10^{17}$', fontsize=10, transform = pl.gca().transAxes)
 pl.legend()
 pl.savefig('images/overlay1.pdf',transparent='true')
 # -------------------------------------
@@ -71,9 +71,9 @@ pl.axes([0.15,0.1,0.95-0.15,0.95-0.1])
 pl.imshow(dens1/1e17,aspect='auto')
 ax = pl.gca()
 ax.xaxis.set_label_coords(0.5, -0.05)
-pl.xlabel('$y$\,[nm]')
-pl.ylabel('$x$\,[nm]')
-pl.text(0.85, -0.55, r'$n\times 10^{17}$', fontsize=8, transform = pl.gca().transAxes)
+pl.xlabel('$y$ [nm]')
+pl.ylabel('$x$ [nm]')
+pl.text(0.85, -0.55, r'$n\times 10^{17}$', fontsize=10, transform = pl.gca().transAxes)
 pl.colorbar(orientation='horizontal')
 pl.savefig('images/dens1.pdf',transparent='true')
 # -------------------------------------
@@ -83,11 +83,11 @@ error=(y4-y3)*100/(sum(y3)/len(x))
 pl.axes([0.15,0.2,0.95-0.15,0.95-0.2])
 pl.plot(x,error,'-r',label=r'Error')
 pl.xlabel(r'$y$ [nm]')
-pl.ylabel(r'relative error [\%]')
+pl.ylabel(r'relative error [\%] $\times 10^{-11}$')
 ax = pl.gca()
 ax.yaxis.set_label_coords(xshift, 0.5)
 locs,labels = pl.yticks()
 pl.yticks(locs, map(lambda x: r"$ %.1f $" % x, locs*1e11))
-pl.text(-0.19, 0.9, r'$\times 10^{-11}$', fontsize=8, transform = pl.gca().transAxes)
-pl.legend()
+# pl.text(-0.19, 0.9, r'$\times 10^{-11}$', fontsize=10, transform = pl.gca().transAxes)
+# pl.legend()
 pl.savefig('images/error1.pdf',transparent='true')
